142e291ccb94ec852c923bbb22b0ca5a43853239
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-05-23 18:01:03 -0300

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,9 @@ Rails.application.configure do
   # config.action_mailer.delivery_method     = :postmark
   # config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
   # config.action_mailer.default_url_options = { host: "yourapp.herokuapp.com" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "igeo-app.herokuapp.com" }
+  # or your custom domain name eg. "www.yourdomain.com"
+
 end
