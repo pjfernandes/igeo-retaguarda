cf37813a9421fefe3e304e23233067716b55b472
Your New Name <Your new email>
2022-07-25 21:29:35 -0300

    done

--- app/javascript/plugins/init_mapbox_for_new_point.js
+++ app/javascript/plugins/init_mapbox_for_new_point.js
@@ -1,59 +1,20 @@
 import mapboxgl from '!mapbox-gl';
 
-let coords;
-
-if (document.querySelector(".map-edit-point")) {
-
-  const latitude = parseFloat(document.querySelector("#point_latitude").value);
-  const longitude = parseFloat(document.querySelector("#point_longitude").value);
-  coords = [longitude, latitude];
-
-} else {
-
-  coords = [-43, -22];
-
-}
-
-console.log(coords);
-
-const returnCoords = () => {
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  function success(pos) {
-    coords = [pos.coords.longitude, pos.coords.latitude];
-    //console.log(coords);
-    //return coords;
-  };
-
-  function error(err) {
-    let coords;
-    if (document.querySelector(".map-edit-point")) {
-
-      const latitude = parseFloat(document.querySelector("#point_latitude").value);
-      const longitude = parseFloat(document.querySelector("#point_longitude").value);
-      coords = [longitude, latitude];
-
-    } else {
-
-      coords = [-43, -22];
-
-    }
-    coords;
-  };
-
-  navigator.geolocation.getCurrentPosition(success, error, options);
-
-};
-
-returnCoords();
-
 function buildMapNewPoint() {
+  var coords;
 
-  let coords;
+  // function getLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.watchPosition(showPosition);
+  //   } else {
+  //     coords = "Geolocation is not supported by this browser.";
+  //   }
+  // }
+
+  // function showPosition(position) {
+  //   coords = [position.coords.latitude, position.coords.longitude];
+
+  // }
 
   if (document.querySelector(".map-edit-point")) {
 
@@ -63,9 +24,9 @@ function buildMapNewPoint() {
 
   } else {
 
-    coords = [-43, -22];
-
+    coords =[-43, -22];
   }
+
   mapboxgl.accessToken = 'pk.eyJ1IjoicGpmZXJuYW5kZXMiLCJhIjoiY2t1c291Z3lzNWg2bzJvbW5kNWNhbnZhaCJ9.eYxvagOUGuS5qDo-zOfRCA';
 
   if (document.getElementById("new_point") || document.getElementById("map-new-point")) {
@@ -73,11 +34,12 @@ function buildMapNewPoint() {
       container: 'map-new-point',
       style: "mapbox://styles/mapbox/satellite-v9",
       center: coords,
-      zoom: 8
+      zoom: 6
     });
 
     var el = document.createElement('div');
     el.id = 'marker';
+
     var marker = new mapboxgl.Marker();
     marker
       .setLngLat(coords)
